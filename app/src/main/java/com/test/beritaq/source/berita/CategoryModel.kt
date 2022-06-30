cca4364ced80e7b505bc33104c45ffb8514e2886
azerid <antareza08@gmail.com>
2022-06-30 10:59:59 +0700

    add category model

---
+++ app/src/main/java/com/test/beritaq/source/berita/CategoryModel.kt
@@ -0,0 +1,6 @@
+package com.test.beritaq.source.berita
+
+data class CategoryModel(
+    val id: String,
+    val nama: String
+)
--- app/src/main/java/com/test/beritaq/source/ui/home/MainViewModel.kt
+++ app/src/main/java/com/test/beritaq/source/ui/home/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.test.beritaq.source.ui.home
 
 import androidx.lifecycle.ViewModel
 import com.test.beritaq.source.berita.BeritaRepository
+import com.test.beritaq.source.berita.CategoryModel
 import org.koin.dsl.module
 
 val mainViewModel = module {
@@ -15,4 +16,15 @@ class MainViewModel(
 ) : ViewModel() {
 
     val title = "Berita"
+
+    val kategories = listOf<CategoryModel>(
+        CategoryModel("", "Berita Utama"),
+        CategoryModel("business", "Bisnis"),
+        CategoryModel("entertainment", "Hiburan"),
+        CategoryModel("general", "Umum"),
+        CategoryModel("health", "Kesehatan"),
+        CategoryModel("science", "Sains"),
+        CategoryModel("sport", "Olah Raga"),
+        CategoryModel("technology", "Teknologi")
+    )
 }
