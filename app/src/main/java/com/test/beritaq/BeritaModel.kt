52f0d157dc0e3ad3e5a5ca7d9b8cb86b79e4aa01
azerid <antareza08@gmail.com>
2022-06-30 10:04:11 +0700

    Add data class Berita Model

---
+++ app/src/main/java/com/test/beritaq/BeritaModel.kt
@@ -0,0 +1,23 @@
+package com.test.beritaq
+
+data class BeritaModel(
+    val status: String,
+    val totalResults: Int,
+    val article: List<ArticleModel>
+)
+
+data class ArticleModel(
+    val source: SourceModel?,
+    val author: String?,
+    val title: String?,
+    val description: String?,
+    val url: String?,
+    val urlToImg: String?,
+    val publishedAt: String,
+    val content: String?
+)
+
+data class SourceModel(
+    val id: String?,
+    val name: String?,
+)
